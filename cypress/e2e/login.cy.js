63c9a9fe4cb90522e3c93b27d421418daba92c63
dnribeiro <davinunesribeiro@gmail.com>
2024-11-28 14:31:07 -0300

    teste completo login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -31,4 +31,34 @@ describe('Teste funcional de login', () => {
         .should("be.visible")
         .and("have.text", "Epic sadface: Sorry, this user has been locked out.")
     });
+
+    it('Deve inserir um usuário errado com senha correta e o mesmo deve ser alertado', () => {
+        // ACT
+        cy.get('[data-test="username"]').type("standard_use")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('[data-test="login-button"]').click()
+
+        // ASSERT
+        cy.get('.error-message-container')
+        .should("have.class", "error")
+        
+        cy.get('[data-test="error"]')
+        .should("be.visible")
+        .and("have.text", "Epic sadface: Username and password do not match any user in this service")
+    });
+
+    it('Deve inserir um usuário correto com senha errada e o mesmo deve ser alertado', () => {
+        // ACT
+        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="password"]').type("secret_sauc")
+        cy.get('[data-test="login-button"]').click()
+
+        // ASSERT
+        cy.get('.error-message-container')
+        .should("have.class", "error")
+        
+        cy.get('[data-test="error"]')
+        .should("be.visible")
+        .and("have.text", "Epic sadface: Username and password do not match any user in this service")
+    });
 });
